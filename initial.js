cd148725b1d03f72d358500411c776727fb97ed9
Olga Smel <yalo245@gmail.com>
2021-03-29 18:30:12 -0800

    initial

--- initial.js
+++ initial.js
@@ -4,7 +4,8 @@ describe('My Little Hero', function () { //define suite title by passing a strin
     describe('Getting to the page', function () { //define sub-suite title by passing a string
 
         it('TC-001 Title is correct ', function () { //define test title by passing a string
-            browser.url('https://qa-apps.netlify.app/hero'); //open baseUrl
+            browser.url('https://qa-apps.netlify.app/hero/fix' +
+                ''); //open baseUrl
             let title = browser.getTitle(); //get page title and assign it to the "title" variable
             browser.pause(2000); //just pause to visually see that something is happening on the page
             expect(title).toEqual('MLH trial'); //compare {title} (actual) and "MLH trial" (expected)
